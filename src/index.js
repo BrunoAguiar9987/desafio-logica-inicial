4cfd1145de6f4519bb1f36058b8dfb9cdff51f9f
BrunoAguiar9987 <bruno.olliveira9987@gmail.com>
2025-05-03 08:03:09 -0300

    Desafio de codigo do felipao  DIO pronto

---
+++ src/index.js
@@ -0,0 +1,34 @@
+let herois = ["the flash" , "superman", "pantera-negra", "mulher-maravilha", "batman", "aquaman" , "hulk", "super-chock" ] 
+
+let pontuacaoXp = [995, 1075, 4700, 5950, 7669, 8243, 9900, 10245 ];
+let nivel = ["ferro", "bronze", "prata", "ouro", "platina", "ascendete", "imortal", "radiante" ]
+
+let heroiEscolhido = herois[7]
+let nivelheroiEscolhido = nivel[7]
+
+ 
+if (herois[7]) {
+         
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+    
+ } else if (heroi[1]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+     
+ } else if (heroi[2]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+
+ } else if (heroi[3]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+
+ } else if (heroi[4]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+
+ } else if (heroi[5]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+
+ } else if (heroi[6]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+
+ } else if (heroi[7]) {
+    console.log( "O heroi de nome " + "''" + heroiEscolhido +  "''" + " esta no nivel " + nivelheroiEscolhido );
+}
